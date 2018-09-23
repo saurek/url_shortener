846d2bc517007a41be4e3cbc1ccebdb7ffca80cd
saurek <s.reklaitis@gmail.com>
2018-09-23 15:02:36 +0300

    2 - Added Model for URL's

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 
+
+class KrstURL(models.Model):
+    url = models.CharField(max_length=220, )
+
+    def __str__(self):
+        return str(self.url)
+
+    def __unicode__(self):
+        return str(self.url)
