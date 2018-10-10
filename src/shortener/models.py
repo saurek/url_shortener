bd6cf2870986183e64230e5ec4c071f067c228b9
saurek <s.reklaitis@gmail.com>
2018-10-10 15:54:22 +0300

    5 - Override the Save Method

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,4 +1,10 @@
+import random
+import string
 from django.db import models
+
+
+def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 
 class KrstURL(models.Model):
@@ -6,6 +12,11 @@ class KrstURL(models.Model):
     shortcode = models.CharField(max_length=15, unique=True)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        print("Hail Satan")
+        self.shortcode = code_generator()
+        super(KrstURL, self).save(*args, **kwargs)
 
     def __str__(self):
         return str(self.url)
